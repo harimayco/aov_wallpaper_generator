0f8df5f9db2d953bb1c2142e2fde795ff699c2ae
Rendy Harimayco <rendyharimayco@gmail.com>
2019-03-26 11:28:10 +0700

    add terser minifier

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,21 @@
 const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
   entry: './js/app.js',
   output: {
     filename: 'app.min.js',
     path: path.resolve(__dirname, 'js')
+  },
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          output: {
+            comments: false,
+          },
+        },
+      }),
+    ],
   }
 };
